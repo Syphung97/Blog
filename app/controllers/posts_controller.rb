33b60269bcd316103c3bf6dbf289ff3290fc322f
Sy Phung <syphung97@gmail.com>
2018-06-06 13:13:35 +0700

    comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
   before_action :logged_in_author, only: [:create, :destory]
+
+  def index
+    if params[:term]
+      @post = Post.search("%#{params[:term]}%")
+    end
+  end
+
   def create
     @post = current_author.posts.build post_params
     if post.save
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,18 @@
 class Post < ApplicationRecord
+  include PgSearch
   enum catalogs: [:html,:css,:javascript,:ruby]
+
   belongs_to :author
+
   has_many :comments
+
   validates :author_id, presence: true
   validates :title, presence: true, length: { maximum:50}
   validates :content, presence: true, length:{maximum: 500}
   validates :catalog, inclusion: {in: catalogs.keys}
+
   mount_uploader :picture, PictureUploader
+
   scope :desc, ->{order created_at: :desc}
   scope :following_feed, (lambda do |following_ids, id|
     where "author_id IN (?) OR author_id = ?", following_ids, id
@@ -14,6 +20,7 @@ class Post < ApplicationRecord
   scope :not_following_feed, (lambda do |following_ids, id|
     where "author_id NOT IN (?) OR author_id != ?", following_ids, id
   end)
-  # Ex:- scope :active, -> {where(:active => true)}
-
+  scope :search, (lambda do |term|
+    where "title LIKE  ? ", term
+  end)
 end
